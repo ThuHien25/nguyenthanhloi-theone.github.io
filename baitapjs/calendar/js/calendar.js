65cd9113f9d89d4dff24a497efeddc26f9def02e
nguyen thanh loi <nguyenthanhloi.theone@gmail.com>
2017-08-24 16:07:07 +0700

    JAVASCRIPT

--- baitapjs/calendar/js/calendar.js
+++ baitapjs/calendar/js/calendar.js
@@ -1,31 +1,119 @@
 /*
-	Feature Calendar
-	Auth: Nguyen Thanh Loi
-*/
-
+ *Feature Calendar
+ *Auth: Nguyen Thanh Loi
+ *Idea: write 1 function create calendar follow to value put in
+ *year , month.
+ */
 var now = new Date();
-var curMon = now.getMonth();
-var curDay = now.getDate();
-var curYear = now.getFullYear();
-var arrWeekdays = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue", "Web"];
-var calendar = document.getElementsByTagName("td"); 
+var cur_Mon = now.getMonth();
+var cur_Day = now.getDate();
+var cur_Year = now.getFullYear();
+var calendar = document.getElementsByTagName("td");
 var demo = document.getElementById("demo");
-createCalendar(curYear,curMon);
+var show_Year = document.getElementById('select_year');
+var show_Month = document.getElementById('select_month');
+createCalendar(cur_Year, cur_Mon);
+slYear();
+chooseDay();
 
-
-//function create calendar
-function createCalendar(valueYear,valueMon) {
-
-	var firstDay = new Date(valueYear,valueMon,1).getDay();
-	var lastDate = new Date(valueYear,valueMon+1,0).getDate();
-	var i;
-	var iDay = 13 + firstDay;
-	for (i = 1; i <= lastDate; i++) {
-		calendar[iDay].innerHTML = i;
-		iDay++;
-	}
-	demo.innerHTML = lastDate;
-
+/*
+ *Create alendar follow to Year , Month put in function
+ *@param valueYear: year
+ *@param valueMon: Month
+ */
+function createCalendar(valueYear, valueMon) {
+    parseInt(valueYear);
+    parseInt(valueMon);
+    var firstDay = new Date(valueYear, valueMon, 1).getDay(); // First Day Of Month
+    var lastDate = new Date(valueYear, valueMon + 1, 0).getDate(); // Total Day Of Month
+    var i;
+    var iDay = 13 + firstDay; // Location start show, 13 is first location show calendar
+    for (i = 13; i < 55; i++) {//Create calendar empty
+        calendar[i].innerHTML = "";
+        calendar[i].style.background = "white";
+    }
+    for (i = 1; i <= lastDate; i++) {//Write calendar
+        calendar[iDay].innerHTML = i;
+        iDay++;
+    }
+    if (valueYear === now.getFullYear()) {//Show Time Current
+        if (valueMon === now.getMonth()) {
+            cDay = 12 + firstDay + now.getDate();
+            calendar[cDay].style.background = "red";
+        }
+    }
+    curTime();
 }
 
-// function click prev or next month
+/*
+ *Event Click < or > : Prev or Next Month
+ */
+function btnMon(valueMon) {
+    cur_Mon = parseInt(cur_Mon) + parseInt(valueMon);
+    if (cur_Mon < 0) {
+        cur_Mon = 11;
+        cur_Year -= parseInt(1);
+    }
+    if (cur_Mon > 11) {
+        cur_Mon = 0;
+        cur_Year += parseInt(1);
+    }
+    createCalendar(cur_Year, cur_Mon);
+}
+
+/*
+ *Event Click << or >> : Prev or Next Year
+ */
+function btnYear(valueYear) {
+    cur_Year = parseInt(cur_Year) + parseInt(valueYear);
+    createCalendar(cur_Year, cur_Mon);
+}
+
+/*
+ *Show Year for Select Year
+ */
+function slYear() {
+    var i;
+    for (i = 1970; i < 2100; i++) {
+        show_Year.innerHTML += "<option value='" + i + "'>" + i + "</option";
+    }
+    createCalendar(cur_Year, cur_Mon);
+}
+
+/*
+ *Click Year. Value Year -> Calendar Year
+ */
+function pickYear() {
+    cur_Year = parseInt(show_Year.value);
+    createCalendar(cur_Year, cur_Mon);
+}
+
+/*
+ *Click Month. Value Month -> Calendar Month of Year
+ */
+function pickMon() {
+    cur_Mon = parseInt(show_Month.value);
+    createCalendar(cur_Year, cur_Mon);
+}
+
+/*
+ *Current Month , Year Select;
+ */
+function curTime() {
+    show_Year.value = cur_Year;
+    show_Month.value = cur_Mon;
+}
+
+/* Chose Day */
+function chooseDay() {
+    var show_DMY = document.getElementById('show_DMY');
+    var i;
+    for (i = 13; i < 50; i++) {
+        calendar[i].addEventListener("click", function() {
+            var dayCheck = this.innerHTML;
+            if (dayCheck != "") {
+                show_DMY.value = dayCheck + "/" + (cur_Mon + 1) + "/" + cur_Year;
+            }
+        });
+    }
+}
--- baitapjs/slider/js/slider.js
+++ baitapjs/slider/js/slider.js
@@ -1,9 +1,8 @@
 /* 
-	Js Slider
-	Auth: Nguyen Thanh Loi
-	Idea: Display:none all image, 1 image display 
-*/
-
+ *Js Slider
+ *Auth: Nguyen Thanh Loi
+ *Idea: Display:none all image, 1 image display 
+ */
 var initImage = 0;
 var reTimeout;
 var img = document.getElementsByClassName("image-slider");
@@ -11,39 +10,41 @@ var img_mini = document.getElementsByClassName("image-mini");
 showImage(initImage);
 
 /*
-	Show Image, Auto Image 3s
-	@param value_Img : value point image for functon
-*/
+ *Show Image, Auto Image 3s
+ *@param value_Img : value point image for functon
+ */
 function showImage(valueImg) {
-	if (valueImg < 0) {
-		initImage = img.length - 1;
-	}
-	if (valueImg > img.length - 1){
-		initImage = 0;
-	}
-	var i;
-	for (i = 0; i < img.length; i++) {
-		img[i].style.display = "none";
-		img_mini[i].className = "image-mini";
-	}
-	img[initImage].style.display = "block";
-	img_mini[initImage].className += " effect";
-	clearTimeout(reTimeout);
-	reTimeout = setTimeout(function() {showImage(initImage += 1);}, 3000);
+    if (valueImg < 0) {
+        initImage = img.length - 1;
+    }
+    if (valueImg > img.length - 1) {
+        initImage = 0;
+    }
+    var i;
+    for (i = 0; i < img.length; i++) {
+        img[i].style.display = "none";
+        img_mini[i].className = "image-mini";
+    }
+    img[initImage].style.display = "block";
+    img_mini[initImage].className += " effect";
+    clearTimeout(reTimeout);
+    reTimeout = setTimeout(function() {
+        showImage(initImage += 1);
+    }, 3000);
 }
 
 /*
- If click btn next or prev , image will next or prev image
- @param valueClick: -1: prev and +1 next
-*/
+ *If click btn next or prev , image will next or prev image
+ *@param valueClick: -1: prev and +1 next
+ */
 function btnClick(valueClick) {
-	showImage(initImage += valueClick);
+    showImage(initImage += valueClick);
 }
 
 /*
- function image mini current
- @param n : value image current
-*/
- function curMiniImg(n) {
-	showImage(initImage = n)
+ *function image mini current
+ *@param n : value image current
+ */
+function curMiniImg(n) {
+    showImage(initImage = n)
 }
